c26dbeb0af568b7524a536deb5f17ce512b6d004
Thomas Boittin <tboittinpro@gmail.com>
2020-03-24 18:21:24 +0100

    exploration of different types of component inside of about.js

--- movie-app/pages/about.js
+++ movie-app/pages/about.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
-
 //functional component
-const About = () => {
-    const message = 'Hello World'
-    return (
-        <h1>Hello About Page - {message}</h1>
-    )
-}
+// WHEN TO USE:
+// 1. for smaller components
+// 2. reusable components
+// 3. presentional loadComponents, partially right, we can use HOOKS and specify state
+// const About = () => {
+//     const message = 'Hello World'
+//     return (
+//         <h1>Hello About Page - {message}</h1>
+//     )
+// }
 
 // const About = () => {
 //     const message = 'Hello World'
@@ -19,6 +22,14 @@ const About = () => {
 //     return(
 //         <h1>Hello About Page</h1>
 //     )
+// }
+
+// class About extends React.Component {
+//     render () {
+//         return (
+//             <h1>Hello I am a class component</h1>
+//         )
+//     }
 // }
 
 export default About
