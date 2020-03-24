562752617e1da95389377fa802cca3b96f1e0326
--global <tboittinpro@gmail.com>
2020-03-24 19:21:15 +0100

    add style jsx

--- movie-app/pages/index.js
+++ movie-app/pages/index.js
@@ -18,25 +18,34 @@ const Home = () => (
     </Head>
 
     <Navbar />
+    <div className="home-page">
+      <div className="container">
+        <div className="row">
 
-    <div className="container">
-      <div className="row">
-
-        <div className="col-lg-3">
-          <SideMenu />
-        </div>
-
-        <div className="col-lg-9">
-          <Carousel />
-          <div className="row">
-            <MovieList />
+          <div className="col-lg-3">
+            <SideMenu />
           </div>
-          
+
+          <div className="col-lg-9">
+            <Carousel />
+            <div className="row">
+              <MovieList />
+            </div>
+            
+          </div>
         </div>
       </div>
-    </div>
 
+    </div>
+    
     <Footer />
+
+    <style jsx>{`
+      .home-page {
+        padding-top: 80px;
+      }
+    `}
+    </style>
 
   </div>
 )
