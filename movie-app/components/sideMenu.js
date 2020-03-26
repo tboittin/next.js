b67b653419792e73effc353542a9995631c3193f
--global <tboittinpro@gmail.com>
2020-03-26 17:36:13 +0100

    iterate over list of data

--- movie-app/components/sideMenu.js
+++ movie-app/components/sideMenu.js
@@ -4,17 +4,11 @@ const SideMenu = (props) => {
     
     return (
         <>
-            <button onClick={props.clickHandler} className="btn btn-danger">
-                Click Me !
-            </button>
             <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
               <a href="#" className="list-group-item">Category 1</a>
               <a href="#" className="list-group-item">Category 2</a>
               <a href="#" className="list-group-item">Category 3</a>
-            </div>
-            <div className="m-3 text-center">
-                <h1>{props.count}</h1>
             </div>
         </>
     )
