1ce229845c0c0190bd44143280dede4b1b534aa6
--global <tboittinpro@gmail.com>
2020-05-11 16:57:02 +0200

    Modal implementation and close modal

--- movie-app/components/sideMenu.js
+++ movie-app/components/sideMenu.js
@@ -1,7 +1,11 @@
+import Modal from "./modal.js"
+
+
 const SideMenu = (props) => {
     
     return (
         <>
+            <Modal />
             <h1 className="my-4">{props.appName}</h1>
             <div className="list-group">
                 {props.categories.map((category)=>(
